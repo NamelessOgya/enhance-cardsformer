2e32fc12916a4377de52cf0bc18b074155573a9b
ChokosenLoveti <hakuginnnohana@gmail.com>
2025-06-01 10:41:15 +0900

    実装しエラー無くうごくようになったものの、データがサンプリングされない。

--- Cardsformer/experiment/train_simple_model/experiment.py
+++ Cardsformer/experiment/train_simple_model/experiment.py
@@ -6,7 +6,8 @@ import os
 import torch
 import gc
 import wandb
-
+import yaml
+from types import SimpleNamespace
 
 from experiment.train_simple_model.train_policy_model import train_policy_model
 from experiment.util.experiment_util import get_experiment_code
@@ -15,52 +16,49 @@ from experiment.train_simple_model.util.model_util import find_best_policy_model
 
 
 
-####### config ##########
-# 後でhydra化
-DECK_MODE = None # train用の限られたデッキで学習するか。"train", "test", None(全デッキ)
-USE_TEXT_FEATURE = False # text featureを使うかどうか
-
-##########################
-
-def path_generator():
+def path_generator(cfg):
     
     return {
-        "policy_model_save_path":f"experiment/train_simple_model/res/{exp_code}/policy_models",
-        "policy_model_load_path":f"experiment/train_simple_model/res/tmp_train_all/policy_models"
+        "policy_model_save_path":f"experiment/train_simple_model/res/{cfg.expcode}/policy_models",
+        "policy_model_load_path":cfg.policy_model_load_path
     }
 
-TOTAL_POLICY_FRAME = 50000000      #policy modelを学習する際のframe数
-
-PROJ_NAME = "simple_model"
 info = generate_experiment_info()
-exp_code = info["experiment_name"]
-
 
 if __name__ == "__main__":
+
+    with open(
+        "./experiment/train_simple_model/experiment_config/config.yaml", 
+        "r", 
+        encoding="utf-8"
+    ) as f:
+        cfg_dict: dict = yaml.safe_load(f) 
+
+    cfg = SimpleNamespace(**cfg_dict)
+    cfg.epsilon = float(cfg.epsilon)
     ###################
     ## wandb 関連設定
     ###################
-    
 
     # wandb
     info = generate_experiment_info()
     wandb.login(key=info["api_key"])
     wandb.init(
-        project=PROJ_NAME,  # プロジェクト名
+        project=cfg.PROJ_NAME,  # プロジェクト名
         name = info["experiment_name"],
-        config={
-            "TOTAL_POLICY_FRAME": TOTAL_POLICY_FRAME
-        }
+        config=cfg_dict
     )
- 
-    dir_dic = path_generator()
+    
+    dir_dic = path_generator(cfg)
 
+    # ディレクトリをなければ作る
     for key, value in dir_dic.items():
         if not os.path.exists(value):
             os.makedirs(value)
 
     print("policy process start")
 
+    # 参照元の重みがあるならloadする。
     if dir_dic["policy_model_load_path"] != "NONE":
         try:
             best_policy_model = find_best_policy_model(dir_dic["policy_model_load_path"])
@@ -73,10 +71,10 @@ if __name__ == "__main__":
     train_policy_model(
         model_save_dir = dir_dic["policy_model_save_path"],
         policy_model_load_path =  dir_dic["policy_model_load_path"],
-        total_frames = TOTAL_POLICY_FRAME,
         best_policy_model_dir = best_policy_model_dir,
-        deck_mode = DECK_MODE,
-        use_text_feature = USE_TEXT_FEATURE,
+        deck_mode = cfg.DECK_MODE,
+        use_text_feature = cfg.USE_TEXT_FEATURE,
+        cfg = cfg
     )
 
     torch.cuda.empty_cache()
